4ed193aec67deb4313f3e2e35ad7ad8001400764
Rafael Dias <rafaeldias0934@gmail.com>
2024-11-19 17:46:47 -0300

    Add ClientCreateDto and ClientResponseDto for request/response handling

--- src/main/java/com/example/demo_park_api/web/dto/ClientResponseDto.java
+++ src/main/java/com/example/demo_park_api/web/dto/ClientResponseDto.java
@@ -1,4 +1,15 @@
 package com.example.demo_park_api.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Setter @Getter
+@NoArgsConstructor @AllArgsConstructor
 public class ClientResponseDto {
+    private Long id;
+    private String name;
+    private String cpf;
 }
