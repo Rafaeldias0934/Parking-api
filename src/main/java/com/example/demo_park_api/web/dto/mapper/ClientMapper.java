90106d60b8d0ec0f0513077081d235ed3fa7830a
Rafael Dias <rafaeldias0934@gmail.com>
2024-11-21 22:28:27 -0300

    Implement ClientMapper to map entity to DTOs and vice versa

--- src/main/java/com/example/demo_park_api/web/dto/mapper/ClientMapper.java
+++ src/main/java/com/example/demo_park_api/web/dto/mapper/ClientMapper.java
@@ -1,4 +1,19 @@
 package com.example.demo_park_api.web.dto.mapper;
 
+import com.example.demo_park_api.entity.Client;
+import com.example.demo_park_api.web.dto.ClientCreateDto;
+import com.example.demo_park_api.web.dto.ClientResponseDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ClientMapper {
+    public static Client toClient(ClientCreateDto clientCreateDto) {
+        return new ModelMapper().map(clientCreateDto, Client.class);
+    }
+
+    public static ClientResponseDto toDto(Client client) {
+        return new ModelMapper().map(client, ClientResponseDto.class);
+    }
 }
