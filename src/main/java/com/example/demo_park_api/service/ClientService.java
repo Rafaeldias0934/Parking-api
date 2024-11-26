6d6f383e8a47615a2fae13fce6b1801ddc44a058
Rafael Dias <rafaeldias0934@gmail.com>
2024-11-26 18:10:40 -0300

    Integrate pagination in ClientService and ClientController

--- src/main/java/com/example/demo_park_api/service/ClientService.java
+++ src/main/java/com/example/demo_park_api/service/ClientService.java
@@ -2,11 +2,17 @@ package com.example.demo_park_api.service;
 
 import com.example.demo_park_api.entity.Client;
 import com.example.demo_park_api.exception.CpfUniqueViolationException;
+import com.example.demo_park_api.exception.EntityNotFoundException;
 import com.example.demo_park_api.repositories.ClientRepository;
+import com.example.demo_park_api.repositories.projection.ClientProjection;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -17,11 +23,24 @@ public class ClientService {
     @Transactional
     public Client toSave(Client client) {
         try {
-            return  clientRepository.save(client);
+            return clientRepository.save(client);
         } catch (DataIntegrityViolationException ex) {
             throw new CpfUniqueViolationException(
                     String.format("CPF '%s' can't be registered, already exists on the system", client.getCpf())
             );
         }
+    }
+
+    @Transactional(readOnly = true)
+    public Client getById(Long id) {
+        return clientRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Client id=%s not found in the system", id)
+                ));
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientProjection> getAll(Pageable pageable) {
+        return clientRepository.findAllPageable(pageable);
+
     }
 }
--- src/main/java/com/example/demo_park_api/web/controller/ClientController.java
+++ src/main/java/com/example/demo_park_api/web/controller/ClientController.java
@@ -2,12 +2,15 @@ package com.example.demo_park_api.web.controller;
 
 import com.example.demo_park_api.entity.Client;
 import com.example.demo_park_api.jwt.JwtUserDetails;
+import com.example.demo_park_api.repositories.projection.ClientProjection;
 import com.example.demo_park_api.service.ClientService;
 import com.example.demo_park_api.service.UserService;
 import com.example.demo_park_api.web.dto.ClientCreateDto;
 import com.example.demo_park_api.web.dto.ClientResponseDto;
+import com.example.demo_park_api.web.dto.PageableDto;
 import com.example.demo_park_api.web.dto.UserResponseDto;
 import com.example.demo_park_api.web.dto.mapper.ClientMapper;
+import com.example.demo_park_api.web.dto.mapper.PageableMapper;
 import com.example.demo_park_api.web.exception.ErrorMessage;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -16,14 +19,15 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "Clients", description = "All operations related to client management")
 @RequiredArgsConstructor
@@ -56,5 +60,30 @@ public class ClientController {
         clientService.toSave(client);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(ClientMapper.toDto(client));
+    }
+
+    @Operation(summary = "Get user by ID", description = "Resource for retrieving a user their unique ID. " +
+            "Request requires the use of a bearer token. Access is restricted to the 'ADMIN' role",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "Resource retrieving successfully",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation = ClientResponseDto.class))),
+                    @ApiResponse(responseCode = "404", description = "Resource not found. No user exists with the provided ID",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class))),
+                    @ApiResponse(responseCode = "403", description = "Resource access is forbidden to the CLIENT role",
+                            content = @Content(mediaType = "application/json;charset=UTF-8", schema = @Schema(implementation = ErrorMessage.class)))
+            })
+
+    @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<ClientResponseDto> getById(@PathVariable Long id) {
+        Client client = clientService.getById(id);
+        return ResponseEntity.ok(ClientMapper.toDto(client));
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<PageableDto> getAll(Pageable pageable) {
+        Page<ClientProjection> clients = clientService.getAll(pageable);
+        return ResponseEntity.ok(PageableMapper.pageableDto(clients));
     }
 }
