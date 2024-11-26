b04c520df355bf58311220fc7c7f15e8cb29224a
Rafael Dias <rafaeldias0934@gmail.com>
2024-11-25 22:48:20 -0300

    Add custom projection support in ClientRepository

--- src/main/java/com/example/demo_park_api/repositories/ClientRepository.java
+++ src/main/java/com/example/demo_park_api/repositories/ClientRepository.java
@@ -1,7 +1,14 @@
 package com.example.demo_park_api.repositories;
 
 import com.example.demo_park_api.entity.Client;
+import com.example.demo_park_api.repositories.projection.ClientProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    @Query("select c from Client c")
+    Page<ClientProjection> findAllPageable(Pageable pageable);
 }
--- src/main/java/com/example/demo_park_api/repositories/projection/ClientProjection.java
+++ src/main/java/com/example/demo_park_api/repositories/projection/ClientProjection.java
@@ -1,4 +1,8 @@
 package com.example.demo_park_api.repositories.projection;
 
 public interface ClientProjection {
+
+    long getId();
+    String getName();
+    String getCpf();
 }
