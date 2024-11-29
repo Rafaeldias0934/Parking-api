75e7866a9d09b9035c9a490a1d94f509451561ed
Rafael Dias <rafaeldias0934@gmail.com>
2024-11-28 23:42:05 -0300

    Add PageableDto and PageableMapper for pagination responses

--- src/main/java/com/example/demo_park_api/web/dto/PageableDto.java
+++ src/main/java/com/example/demo_park_api/web/dto/PageableDto.java
@@ -1,4 +1,29 @@
 package com.example.demo_park_api.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
 public class PageableDto {
+
+    private List content = new ArrayList<>();
+
+    private boolean first;
+
+    private boolean last;
+
+    private int totalPages;
+
+    @JsonProperty("page")
+    private int number;
+
+    private int size;
+
+    @JsonProperty("pageElements")
+    private int numberOfElements;
+    private int totalElements;
 }
--- src/main/java/com/example/demo_park_api/web/dto/mapper/PageableMapper.java
+++ src/main/java/com/example/demo_park_api/web/dto/mapper/PageableMapper.java
@@ -1,4 +1,15 @@
 package com.example.demo_park_api.web.dto.mapper;
 
+import com.example.demo_park_api.web.dto.PageableDto;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Page;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class PageableMapper {
+
+    public  static PageableDto pageableDto(Page page) {
+        return new ModelMapper().map(page, PageableDto.class);
+    }
 }
